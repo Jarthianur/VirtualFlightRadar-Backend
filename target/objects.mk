e677caf091ef1f08768c74815a23ce02ff9afb1e
Jarthianur <julian.jack@hotmail.de>
2017-02-18 18:15:42 +0100

    tmp rm target

--- target/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_thread -lpthread -lboost_signals -lboost_system -lboost_chrono -lboost_regex
-
