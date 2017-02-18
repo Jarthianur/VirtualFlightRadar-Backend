dd361b9c51324c723b5fd71ecee954743facf0a5
Jarthianur <julian.jack@hotmail.de>
2017-02-18 18:20:52 +0100

    gitignore issue

---
+++ target/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_thread -lpthread -lboost_signals -lboost_system -lboost_chrono -lboost_regex
+
---
+++ target/src/config/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/config/ConfigReader.cpp \
+../src/config/Configuration.cpp 
+
+OBJS += \
+./src/config/ConfigReader.o \
+./src/config/Configuration.o 
+
+CPP_DEPS += \
+./src/config/ConfigReader.d \
+./src/config/Configuration.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/config/%.o: ../src/config/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	$(CXX) -std=c++0x $(BOOST_I) -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
