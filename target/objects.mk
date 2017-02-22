9dfab1914d987cfbc843c3ac9e43f5bb298f8144
Jarthianur <julian.jack@hotmail.de>
2017-02-22 10:00:43 +0100

    try ignores in target

--- target/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lboost_regex -lboost_chrono -lboost_system -lpthread -lboost_thread -lboost_signals
-
