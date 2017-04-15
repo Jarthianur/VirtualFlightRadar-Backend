bc55d9e2c116de0bc11c6a386ca78a8d6b993fcb
Jarthianur <Jarthianur@users.noreply.github.com>
2017-04-15 20:58:55 +0200

    a

--- src/parser/GPSParser.cpp
+++ src/parser/GPSParser.cpp
@@ -45,7 +45,7 @@ std::int32_t GPSParser::unpack(const std::string& msg) noexcept
 {
     try
     {
-        std::int32_t csum = std::stoi(msg.substr(msg.length() - 3, 2), nullptr, 16);
+        std::int32_t csum = std::stoi(msg.substr(msg.length() - 2, 2), nullptr, 16);
         if (csum != Math::checksum(msg.c_str(), msg.length()))
         {
             return MSG_UNPACK_IGN;
