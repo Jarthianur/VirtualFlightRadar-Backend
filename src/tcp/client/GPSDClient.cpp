1d75838d6b05a2e7930e1854a0d4fc8fe56c04ad
Jarthianur <Jarthianur@users.noreply.github.com>
2017-04-15 17:26:19 +0200

    add gps classes

---
+++ src/tcp/client/GPSDClient.cpp
@@ -0,0 +1,136 @@
+/*
+ Copyright_License {
+
+ Copyright (C) 2017 VirtualFlightRadar-Backend
+ A detailed list of copyright holders can be found in the file "AUTHORS".
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License version 3
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ }
+ */
+
+#include "GPSDClient.h"
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/system/error_code.hpp>
+#include <cstddef>
+#include <iostream>
+
+#include "../../util/Logger.h"
+
+GPSDClient::GPSDClient(boost::asio::signal_set& sigset, const std::string& host,
+                       const std::string& port)
+        : Client(sigset, host, port, "(GPSDClient)"),
+          mParser()
+{
+    connect();
+}
+
+GPSDClient::~GPSDClient() noexcept
+{
+}
+
+void GPSDClient::connect() noexcept
+{
+    boost::asio::ip::tcp::resolver::query query(
+            mHost, mPort, boost::asio::ip::tcp::resolver::query::canonical_name);
+    mResolver.async_resolve(
+            query,
+            boost::bind(&GPSDClient::handleResolve, this,
+                        boost::asio::placeholders::error,
+                        boost::asio::placeholders::iterator));
+}
+
+void GPSDClient::process() noexcept
+{
+    mParser.unpack(mResponse);
+}
+
+void GPSDClient::handleResolve(const boost::system::error_code& ec,
+                               boost::asio::ip::tcp::resolver::iterator it) noexcept
+{
+    if (!ec)
+    {
+        boost::asio::async_connect(
+                mSocket,
+                it,
+                boost::bind(&GPSDClient::handleConnect, this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::iterator));
+    }
+    else
+    {
+        Logger::error("(GPSDClient) resolve host: ", ec.message());
+        if (mSocket.is_open())
+        {
+            mSocket.close();
+        }
+        timedConnect();
+    }
+}
+
+void GPSDClient::handleConnect(const boost::system::error_code& ec,
+                               boost::asio::ip::tcp::resolver::iterator it) noexcept
+{
+    if (!ec)
+    {
+        mSocket.set_option(boost::asio::socket_base::keep_alive(true)); // necessary?
+        boost::asio::async_write(
+                mSocket,
+                boost::asio::buffer("?WATCH={\"enable\":true,\"nmea\":true}\r\n"),
+                boost::bind(&GPSDClient::handleWatch, this,
+                            boost::asio::placeholders::error,
+                            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+        Logger::error("(GPSDClient) connect: ", ec.message());
+        if (mSocket.is_open())
+        {
+            mSocket.close();
+        }
+        timedConnect();
+    }
+}
+
+void GPSDClient::stop() noexcept
+{
+    boost::asio::async_write(
+            mSocket, boost::asio::buffer("?WATCH={\"enable\":false}\r\n"),
+            [this](boost::system::error_code& ec, std::size_t s)
+            {
+                if (!ec)
+                {
+                    Logger::info("(GPSDClient) stopped watch");
+                }
+                else
+                {
+                    Logger::error("(GPSDClient) send un-watch request: ", ec.message());
+                }
+            });
+    Client::stop();
+}
+
+void GPSDClient::handleWatch(const boost::system::error_code& ec, std::size_t s) noexcept
+{
+    if (!ec)
+    {
+        Logger::info("(GPSDClient) connected to: ", mHost);
+        read();
+    }
+    else
+    {
+        Logger::error("(GPSDClient) send watch request: ", ec.message());
+    }
+}
---
+++ src/tcp/client/GPSDClient.h
@@ -0,0 +1,62 @@
+/*
+ Copyright_License {
+
+ Copyright (C) 2017 VirtualFlightRadar-Backend
+ A detailed list of copyright holders can be found in the file "AUTHORS".
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License version 3
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ }
+ */
+
+#ifndef SRC_TCP_CLIENT_GPSDCLIENT_H_
+#define SRC_TCP_CLIENT_GPSDCLIENT_H_
+
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+#include <string>
+
+#include "../../parser/GPSParser.h"
+#include "Client.h"
+
+class GPSDClient: public Client
+{
+public:
+    GPSDClient(const GPSDClient&) = delete;
+    GPSDClient& operator=(const GPSDClient&) = delete;
+
+    GPSDClient(boost::asio::signal_set& /*sigset*/, const std::string& /*host*/,
+               const std::string& /*port*/);
+    virtual ~GPSDClient() noexcept;
+
+private:
+    void process() noexcept override;
+    void connect() noexcept override;
+    void stop() noexcept override;
+
+    void handleResolve(const boost::system::error_code& /*ec*/,
+                       boost::asio::ip::tcp::resolver::iterator /*it*/) noexcept override;
+    void handleConnect(const boost::system::error_code& /*ec*/,
+                       boost::asio::ip::tcp::resolver::iterator /*it*/) noexcept override;
+    /**
+     * Send watch string - handler
+     */
+    void handleWatch(const boost::system::error_code& /*ec*/, std::size_t /*s*/) noexcept;
+
+    /**
+     * Parser
+     */
+    GPSParser mParser;
+};
+
+#endif /* SRC_TCP_CLIENT_GPSDCLIENT_H_ */
