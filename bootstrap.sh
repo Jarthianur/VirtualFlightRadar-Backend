7326e9b35f4dc6d2efcc1c59e02a594b7293f78e
Jarthianur <jarthianur.github@gmail.com>
2018-03-17 19:51:59 +0100

    ready so far

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ export SUDO="${SUDO:-}"
 
 function ifelse() {
     error=0
-    if [ $1 -eq 0 ]; then
+    if [ $1 ]; then
         printf '%s' "$2"
     else
         printf '%s' "$3"
@@ -15,6 +15,8 @@ function ifelse() {
     fi
     return $error
 }
+
+export PROMPT="$(basename $0)`ifelse "-z $1" '' "->$1"`"
 
 function log() {
     TIME=`date +"%T"`
@@ -28,7 +30,7 @@ function log() {
         echo -en "\033[0;31m[WARN]\033[0m"
     ;;
     esac
-    shift;
+    shift
     echo ' '"$TIME $PROMPT:: $*"
 }
 
@@ -43,7 +45,38 @@ function require() {
     return $error
 }
 
+function retval() {
+    "$*" &> /dev/null
+    echo $?
+}
+
+function fail() {
+    $1
+    shift
+    log -e $*
+    exit 1
+}
+
+function confirm() {
+    log -i $*
+    if [ ! -z "$AUTO_CONFIRM" ]; then
+        read -r -p "Confirm? [yes|no]: " READ
+        echo ''
+        case $READ in
+        [yY] | yes | YES)
+            return 0
+        ;;
+        *)
+            return 1
+        ;;
+        esac
+    fi
+    log -w Automatically confirmed.
+    return 0
+}
+
 function resolve_pkg_manager() {
+    set +e
     error=0
     if [ "$(which 2>&1)" == "" ]; then
         APT="$(which apt-get)"
@@ -71,7 +104,7 @@ function resolve_pkg_manager() {
 
 function install_deps() {
     set -e
-    log -i "... INSTALL DEPENDENCIES ..."
+    log -i INSTALL DEPENDENCIES
     resolve_pkg_manager
     require PKG_MANAGER
     case $PKG_MANAGER in
@@ -80,15 +113,20 @@ function install_deps() {
         SETUP=""
         BOOST='libboost-dev libboost-all-dev'
         PYTHON='python python-pip'
+        GCC='g++ g++-multilib'
     ;;
     yum)
         UPDATE='yum clean all'
         SETUP="yum -y install epel-release"
         BOOST='boost boost-devel'
         PYTHON='python python-pip'
+        GCC='gcc-c++'
     ;;
     esac
-    ALL="$BOOST $PYTHON"
+    ALL="$GCC $PYTHON"
+    if [ -z "$CUSTOM_BOOST" ]; then
+        ALL="$ALL $BOOST"
+    fi
     $SUDO $SETUP
     $SUDO $UPDATE
     log -i "$SUDO" "$PKG_MANAGER" -y install "$ALL"
@@ -96,4 +134,70 @@ function install_deps() {
     pip install --upgrade pip
     pip install spline
     return $error
+}
+
+function prepare_path() {
+    set -e
+    if [ -e "$1" ]; then
+        log -w "\"$1\"" already exists.
+        confirm Replace the existing one\?
+        rm -rf "$1"
+    else
+        mkdir -p "$(dirname $1)"
+    fi
+}
+
+function build() {
+    set -eE
+    log -i BUILD VFRB
+    require VFRB_ROOT VFRB_TARGET VFRB_COMPILER
+    if [ ! -z "$CUSTOM_BOOST" ]; then
+        require BOOST_LIBS_L BOOST_ROOT_I
+    fi
+    export VFRB_OPT=${VFRB_OPT:-"3"}
+    export VFRB_DEBUG=""
+    trap "fail popd Build has failed!" ERR
+    pushd "$VFRB_ROOT/target/"    
+    make all -j2
+    popd
+    trap - ERR
+}
+
+function install_service() {
+    set -eE
+    log -i INSTALL VFRB SERVICE
+    require VFRB_ROOT VFRB_EXEC_PATH VFRB_INI_PATH
+    SYSD_PATH="/etc/systemd/system"
+    trap "fail popd Service installation has failed!" ERR
+    pushd "$SYSD_PATH"
+    if [ ! -z "$CUSTOM_BOOST" ]; then
+        require BOOST_ROOT
+        $SUDO mkdir "vfrb.service.d"
+        $SUDO sh -c "sed -e 's|%BOOST_LIBS_PATH%|${BOOST_ROOT}/stage/lib:|' \
+        <'$VFRB_ROOT/service/vfrb.service.d/vfrb.conf' >vfrb.service.d/vfrb.conf"
+    fi
+    $SUDO sh -c "sed -e 's|%VFRB_EXEC_PATH%|${VFRB_EXEC_PATH}|' \
+        -e 's|%VFRB_INI_PATH%|${VFRB_INI_PATH}|' \
+        < ${VFRB_ROOT}/service/vfrb.service >vfrb.service"
+    $SUDO systemctl enable vfrb.service
+    log -i VFRB service created.
+    trap - ERR
+}
+
+function install() {
+    set -e
+    log -i INSTALL VFRB
+    require VFRB_TARGET VFRB_ROOT VFRB_EXEC_PATH VFRB_INI_PATH VFRB_VERSION REPLACE_INI
+    if [ -x "$VFRB_ROOT/target/$VFRB_TARGET" ]; then
+        mv "$VFRB_ROOT/target/$VFRB_TARGET" "$VFRB_EXEC_PATH"
+        log -i "$VFRB_EXEC_PATH" created.
+    else
+        log -e "$VFRB_TARGET" does not exist!
+        return 1
+    fi
+    if [ $[REPLACE_INI] -eq 0 ]; then
+        sed "s|%VERSION%|${VFRB_VERSION}|" <"$VFRB_ROOT/vfrb.ini" >"$VFRB_INI_PATH"
+        log -i "$VFRB_INI_PATH" created.
+    fi
+    install_service
 }
