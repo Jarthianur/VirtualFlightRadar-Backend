5ae15ae62aab54ebe367724a161740a71f683416
Jarthianur <jarthianur.github@gmail.com>
2018-03-17 16:42:11 +0100

    provide pipeline functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,99 @@
 #!/bin/bash
 
-# name of the executable, service (default: vfrb)
-# example: VFRB
-export VFRB_NAME_B=
+if [ "$(whoami)" != 'root' ]; then
+    SUDO='sudo'
+fi
+export SUDO="${SUDO:-}"
 
-# name of INI file (default: vfrb.ini)
-# file extension will be appended automatically
-# example: VFRB
-export VFRB_INI_B=
+function ifelse() {
+    error=0
+    if [ $1 -eq 0 ]; then
+        printf '%s' "$2"
+    else
+        printf '%s' "$3"
+        error=1
+    fi
+    return $error
+}
 
-# compiler to use (default: g++)
-# may be set to path to cross compiler
-export VFRB_COMPILER_B=
+function log() {
+    TIME=`date +"%T"`
+    case $1 in -i)
+        echo -en "\033[0;32m[INFO]\033[0m"
+    ;;
+    -w)
+        echo -en "\033[0;33m[WARN]\033[0m"
+    ;;
+    -e)
+        echo -en "\033[0;31m[WARN]\033[0m"
+    ;;
+    esac
+    shift;
+    echo ' '"$TIME $PROMPT:: $*"
+}
 
-# set only if boost installed in custom directory
-# example: /home/user/boost/
-export BOOST_ROOT_B=
+function require() {
+    error=0
+    for v in $*; do
+        if [ -z "${!v}" ]; then
+            log -e Env. $v is not set!
+            error=1
+        fi
+    done
+    return $error
+}
 
-# path to executable after installation (default: target/)
-# example: /home/user/bin/ , ~/bin/
-export VFRB_EXEC_PATH_B=
+function resolve_pkg_manager() {
+    error=0
+    if [ "$(which 2>&1)" == "" ]; then
+        APT="$(which apt-get)"
+        YUM="$(which yum)"
+    else
+        RELEASE="$(cat /etc/*-release)"
+        if [ "$(echo $RELEASE | grep -oiP '(ubuntu|debian)')" != "" ]; then
+            APT="apt-get"
+        elif [ "$(echo $RELEASE | grep -oiP '(centos|fedora)')" != "" ]; then
+            YUM="yum"
+        fi
+    fi
+    if [ ! -z "$APT" ]; then
+        export PKG_MANAGER="$APT"
+        log -i Using apt as package manager.
+    elif [ ! -z "$YUM" ]; then
+        export PKG_MANAGER="$YUM"
+        log -i Using yum as package manager.
+    else
+        log -e Could not determine package manager!
+        error=1
+    fi
+    return $error
+}
 
-# path to INI file (default: target/)
-# example: /home/user/ , ~/
-export VFRB_INI_PATH_B=
-
+function install_deps() {
+    set -e
+    log -i "... INSTALL DEPENDENCIES ..."
+    resolve_pkg_manager
+    require PKG_MANAGER
+    case $PKG_MANAGER in
+    apt-get)
+        UPDATE="apt-get update"
+        SETUP=""
+        BOOST='libboost-dev libboost-all-dev'
+        PYTHON='python python-pip'
+    ;;
+    yum)
+        UPDATE='yum clean all'
+        SETUP="yum -y install epel-release"
+        BOOST='boost boost-devel'
+        PYTHON='python python-pip'
+    ;;
+    esac
+    ALL="$BOOST $PYTHON"
+    $SUDO $SETUP
+    $SUDO $UPDATE
+    log -i "$SUDO" "$PKG_MANAGER" -y install "$ALL"
+    $SUDO $PKG_MANAGER -y install $ALL
+    pip install --upgrade pip
+    pip install spline
+    return $error
+}
