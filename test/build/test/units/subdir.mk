3c6e3751fe47f32038683f4bc8d82ca9e7b2805c
Jarthianur <jarthianur.github@gmail.com>
2018-03-23 15:27:46 +0100

    fix build file

--- test/build/test/units/subdir.mk
+++ test/build/test/units/subdir.mk
@@ -4,32 +4,35 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../units/TestAircraft.cpp \
 ../units/TestConfig.cpp \
 ../units/TestData.cpp \
-../units/TestParser.cpp \
-../units/TestUtil.cpp 
+../units/TestDataProcessor.cpp \
+../units/TestFeedParser.cpp \
+../units/TestMath.cpp \
+../units/TestObject.cpp
 
 OBJS += \
-./test/units/TestAircraft.o \
 ./test/units/TestConfig.o \
 ./test/units/TestData.o \
-./test/units/TestParser.o \
-./test/units/TestUtil.o 
+./test/units/TestDataProcessor.o \
+./test/units/TestFeedParser.o \
+./test/units/TestMath.o \
+./test/units/TestObject.o
 
 CPP_DEPS += \
-./test/units/TestAircraft.d \
 ./test/units/TestConfig.d \
 ./test/units/TestData.d \
-./test/units/TestParser.d \
-./test/units/TestUtil.d 
+./test/units/TestDataProcessor.d \
+./test/units/TestFeedParser.d \
+./test/units/TestMath.d \
+./test/units/TestObject.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
 test/units/%.o: ../units/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -fprofile-arcs -ftest-coverage -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	$(CXX) -std=c++0x $(BOOST_I) -O0 -g3 -Wall -c -fmessage-length=0 -fprofile-arcs -ftest-coverage -fopenmp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
