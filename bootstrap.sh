4549ad77053549bd8aff129979463daaab2fd5e8
Jarthianur <jarthianur.github@gmail.com>
2018-04-16 12:48:51 +0200

    install perl for tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -233,7 +233,7 @@ function install_test_deps() {
     require PKG_MANAGER
     case $PKG_MANAGER in
     *apt-get)
-        local TOOLS='cppcheck clang-format-5.0 wget netcat procps'
+        local TOOLS='cppcheck clang-format-5.0 wget netcat procps perl'
     ;;
     *)
         log -w Tests currently only run under ubuntu/debian systems.
