63cac8314cbb9a00c5f5d4afacddf9b8b43c7695
Jarthianur <jarthianur.github@gmail.com>
2018-09-25 12:41:28 +0200

    fix build files

--- build/src/client/subdir.mk
+++ build/src/client/subdir.mk
@@ -4,13 +4,34 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/client/ConnectorImplBoost.cpp 
+../src/client/ConnectorImplBoost.cpp \
+../src/client/Client.cpp \
+../src/client/AprscClient.cpp \
+../src/client/GpsdClient.cpp \
+../src/client/SbsClient.cpp \
+../src/client/SensorClient.cpp \
+../src/client/ClientFactory.cpp \
+../src/client/ClientManager.cpp
 
 OBJS += \
-./src/client/ConnectorImplBoost.o
+./src/client/ConnectorImplBoost.o \
+./src/client/Client.o \
+./src/client/AprscClient.o \
+./src/client/GpsdClient.o \
+./src/client/SbsClient.o \
+./src/client/SensorClient.o \
+./src/client/ClientFactory.o \
+./src/client/ClientManager.o
 
 CPP_DEPS += \
-./src/client/ConnectorImplBoost.d
+./src/client/ConnectorImplBoost.d \
+./src/client/Client.d \
+./src/client/AprscClient.d \
+./src/client/GpsdClient.d \
+./src/client/SbsClient.d \
+./src/client/SensorClient.d \
+./src/client/ClientFactory.d \
+./src/client/ClientManager.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
