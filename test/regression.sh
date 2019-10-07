8ef73f3b2f6aaa6cd13962ed58226fd04afc7f1d
Jarthianur <jarthianur.github@gmail.com>
2019-10-07 22:18:07 +0200

    cleanup trap

--- test/regression.sh
+++ test/regression.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eE
+
 REGR_PORT_SBS=44400
 REGR_PORT_APRS=44401
 REGR_PORT_GPS=44402
@@ -7,8 +9,7 @@ REGR_PORT_VFRB=44404
 
 function cleanup() {
     kill -9 $(cat sbs_srv.pid) $(cat aprs_srv.pid) $(cat gps_srv.pid) $(cat sens_srv.pid)
-    rm *.pid *.log
-    trap - ERR
+    rm sens_srv.pid aprs_srv.pid sbs_srv.pid gps_srv.pid
 }
 
 case "$1" in
@@ -30,48 +31,49 @@ case "$1" in
     LOG=$(cat aprs.log | grep -o "regression")
     if [ "$LOG" == "" ]; then
         echo "APRS regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat gps.log | grep -o "?WATCH={\"enable\":true,\"nmea\":true}")
     if [ "$LOG" == "" ]; then
         echo "VFRB - gps_watch_enable regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat gps.log | grep -o "?WATCH={\"enable\":false}")
     if [ "$LOG" == "" ]; then
         echo "VFRB - gps_watch_disable regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'GPRMC,\d{6},A,5000.05,N' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - gprmc regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'GPGGA,\d{6},5000.0466,N' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - gpgga regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'PFLAU,,,,1,0,-180,0,-105' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - pflau regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'PFLAA,0,-11008362,-33512,-105' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - pflaa regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'WIMDA,29.7987,I,1.0091,B,14.8' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - wimda regression failed"
-        exit 1
+        $(exit 1)
     fi
     LOG=$(cat vfrb.log | grep -o -P 'WIMWV,242.8,R,6.9,N,A' | head -n1)
     if [ "$LOG" == "" ]; then
         echo "VFRB - wimwv regression failed"
-        exit 1
+        $(exit 1)
     fi
+    cleanup
     ;;
 *)
     echo "Run this script with 'serve' to start regression servers."
