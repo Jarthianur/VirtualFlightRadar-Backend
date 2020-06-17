6a945bc706483d68afbd7a24e682572b92dd8887
Jarthianur <jarthianur.github@gmail.com>
2020-06-17 20:19:24 +0000

    adjust some tests

--- test/TestFeedParser.cpp
+++ test/TestFeedParser.cpp
@@ -18,41 +18,40 @@
     along with VirtualFlightRadar-Backend.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "feed/parser/AprsParser.h"
-#include "feed/parser/AtmosphereParser.h"
-#include "feed/parser/GpsParser.h"
-#include "feed/parser/SbsParser.h"
-#include "feed/parser/WindParser.h"
-#include "math/math.hpp"
-#include "object/Aircraft.h"
-#include "object/Atmosphere.h"
-#include "object/GpsPosition.h"
-#include "object/Timestamp.h"
-#include "object/Wind.h"
+#include "feed/parser/CAprsParser.hpp"
+#include "feed/parser/CAtmosphereParser.hpp"
+#include "feed/parser/CGpsParser.hpp"
+#include "feed/parser/CSbsParser.hpp"
+#include "feed/parser/CWindParser.hpp"
+#include "math/Math.hpp"
+#include "object/CAircraft.hpp"
+#include "object/CAtmosphere.hpp"
+#include "object/CGpsPosition.hpp"
+#include "object/CTimestamp.hpp"
+#include "object/CWind.hpp"
 
-#include "helper.hpp"
+#include "Helper.hpp"
+
+using sctf::FEQ;
 
 using namespace vfrb;
 using namespace feed::parser;
-using namespace sctf;
 
-TEST_MODULE(test_SbsParser, {
-    test("valid msg", [] {
+DESCRIBE_PAR("test_CSbsParser") {
+    IT("should parse a valid sentence correctly") {
+        str msg =
+            "MSG,3,0,0,AAAAAA,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,1000,,,49.000000,8.000000,,,,,,0";
         CSbsParser p(100000);
-        ASSERT_NOTHROW(p.Parse(
-            "MSG,3,0,0,AAAAAA,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,1000,,,49.000000,8.000000,,,,,,0",
-            0));
-        auto ac = p.Parse(
-            "MSG,3,0,0,AAAAAA,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,1000,,,49.000000,8.000000,,,,,,0",
-            0);
-        ASSERT_EQUALS(ac.Id(), "AAAAAA");
-        ASSERT_EQUALS(ac.TargetType(), object::CAircraft::ETargetType::TRANSPONDER);
-        ASSERT_EQUALS(ac.Location().Altitude, math::DoubleToInt(math::FEET_2_M * 1000));
-        ASSERT_EQUALS(ac.Location().Latitude, 49.0);
-        ASSERT_EQUALS(ac.Location().Longitude, 8.0);
+        ASSERT_NOTHROW(p.Parse(msg, 0));
+        auto ac = p.Parse(msg, 0);
+        ASSERT_EQ(ac.Id(), "AAAAAA");
+        ASSERT_EQ(ac.TargetType(), object::CAircraft::ETargetType::TRANSPONDER);
+        ASSERT_EQ(ac.Location().Altitude, math::DoubleToInt(math::FEET_2_M * 1000));
+        ASSERT(ac.Location().Latitude, FEQ(), 49.0);
+        ASSERT(ac.Location().Longitude, FEQ(), 8.0);
         ASSERT_FALSE(ac.HasFullInfo());
-    });
-    test("invalid msg", [] {
+    }
+    IT("should throw when parsing invalid sentence") {
         CSbsParser p(100000);
         ASSERT_THROWS(
             p.Parse("MSG,3,0,0,AAAAAA,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,,,,,,,,,,,0", 0),
@@ -75,61 +74,64 @@ TEST_MODULE(test_SbsParser, {
             p.Parse("MSG,3,0,0,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,100#0,,,,,,,,,,0", 0),
             feed::parser::error::CParseError);
         ASSERT_THROWS(p.Parse("", 0), feed::parser::error::CParseError);
-    });
-    test("filter height", [] {
+    }
+    IT("should filter by height") {
         CSbsParser p(0);
         ASSERT_THROWS(
             p.Parse(
                 "MSG,3,0,0,AAAAAA,0,2017/02/16,20:11:30.772,2017/02/16,20:11:30.772,,1000,,,49.000000,8.000000,,,,,,0",
                 0),
             feed::parser::error::CParseError);
-    });
-})
+    };
+};
 
-TEST_MODULE(test_AprsParser, {
-    test("valid msg", [] {
-        CAprsParser p;
-        assertNoExcept(aprsParser.unpack(
-            "FLRAAAAAA>APRS,qAS,XXXX:/100715h4900.00N/00800.00E'/A=000000 !W19! id06AAAAAA", 0));
-        assertNoExcept(aprsParser.unpack(
+DESCRIBE_PAR("test_CAprsParser") {
+    IT("should parse a valid sentence correctly") {
+        CAprsParser p(10000);
+        ASSERT_NOTHROW(
+            p.Parse("FLRAAAAAA>APRS,qAS,XXXX:/100715h4900.00N/00800.00E'/A=000000 !W19! id06AAAAAA", 0));
+        ASSERT_NOTHROW(p.Parse(
             "ICAAAAAAA>APRS,qAR:/081733h4900.00N/00800.00EX180/003/A=000000 !W38! id0DAAAAAA -138fpm +0.0rot 6.2dB 0e +4.2kHz gps4x4",
             0));
-        assertNoExcept(aprsParser.unpack(
+        ASSERT_NOTHROW(p.Parse(
             "FLRAAAAAA>APRS,qAS,XXXX:/100715h4900.00S\\00800.00E^276/014/A=000000 !W07! id22AAAAAA -019fpm +3.7rot 37.8dB 0e -51.2kHz gps2x4",
             0));
-        assertNoExcept(aprsParser.unpack(
+        ASSERT_NOTHROW(p.Parse(
             "FLRAAAAAA>APRS,qAS,XXXX:/074548h4900.00N/00800.00W'000/000/A=000000 id0AAAAAAA +000fpm +0.0rot 5.5dB 3e -4.3kHz",
             0));
-        auto ac = aprsParser.unpack(
+        auto ac = p.Parse(
             "FLRAAAAAA>APRS,qAS,XXXX:/074548h4900.00N/00800.00W'000/000/A=000000 id0AAAAAAA +000fpm +0.0rot 5.5dB 3e -4.3kHz",
             0);
-        assertEquals(ac.getId(), "AAAAAA");
-        assertEquals(ac.getTargetType(), object::Aircraft::TargetType::FLARM);
-        assertEquals(ac.getLocation().altitude, 0);
-        assertEquals(ac.getLocation().latitude, 49.0);
-        assertEquals(ac.getLocation().longitude, -8.0);
-        assertTrue(ac.hasFullInfo());
-    });
-    test("invalid msg", [] {
-        AprsParser       aprsParser;
-        object::Aircraft ac;
-        assertFalse(aprsParser.unpack(
-            "Valhalla>APRS,TCPIP*,qAC,GLIDERN2:/074555h4900.00NI00800.00E&/A=000000 CPU:4.0 RAM:242.7/458.8MB NTP:0.8ms/-28.6ppm +56.2C RF:+38+2.4ppm/+1.7dB",
-            ac));
-        assertFalse(aprsParser.unpack(
-            "# aprsc 2.0.14-g28c5a6a 29 Jun 2014 07:46:15 GMT SERVER1 00.000.00.000:14580", ac));
-        assertFalse(aprsParser.unpack("", ac));
-        assertFalse(aprsParser.unpack("FLRAAAAAA>APRS,qAS,XXXX:/100715h4900.00N/00800.00E'/A=000000 ", ac));
-    });
-    test("filter height", [] {
-        AprsParser tmpAprs;
-        AprsParser::s_maxHeight = 0;
-        object::Aircraft ac;
-        assertFalse(tmpAprs.unpack(
-            "FLRAAAAAA>APRS,qAS,XXXX:/074548h4900.00N/00800.00W'000/000/A=001000 id0AAAAAAA +000fpm +0.0rot 5.5dB 3e -4.3kHz",
-            ac));
-    });
-})
+        ASSERT_EQ(ac.Id(), "AAAAAA");
+        ASSERT_EQ(ac.TargetType(), object::CAircraft::ETargetType::FLARM);
+        ASSERT_EQ(ac.Location().Altitude, 0);
+        ASSERT(ac.Location().Latitude, FEQ(), 49.0);
+        ASSERT(ac.Location().Longitude, FEQ(), -8.0);
+        ASSERT_TRUE(ac.HasFullInfo());
+    }
+    IT("should throw when parsing invalid sentence") {
+        CAprsParser p(10000);
+        ASSERT_THROWS(
+            p.Parse(
+                "Valhalla>APRS,TCPIP*,qAC,GLIDERN2:/074555h4900.00NI00800.00E&/A=000000 CPU:4.0 RAM:242.7/458.8MB NTP:0.8ms/-28.6ppm +56.2C RF:+38+2.4ppm/+1.7dB",
+                0),
+            feed::parser::error::CParseError);
+        ASSERT_THROWS(
+            p.Parse("# aprsc 2.0.14-g28c5a6a 29 Jun 2014 07:46:15 GMT SERVER1 00.000.00.000:14580", 0),
+            feed::parser::error::CParseError);
+        ASSERT_THROWS(p.Parse("", 0), feed::parser::error::CParseError);
+        ASSERT_THROWS(p.Parse("FLRAAAAAA>APRS,qAS,XXXX:/100715h4900.00N/00800.00E'/A=000000 ", 0),
+                      feed::parser::error::CParseError);
+    }
+    IT("should filter by height") {
+        CAprsParser p(0);
+        ASSERT_THROWS(
+            p.Parse(
+                "FLRAAAAAA>APRS,qAS,XXXX:/074548h4900.00N/00800.00W'000/000/A=001000 id0AAAAAAA +000fpm +0.0rot 5.5dB 3e -4.3kHz",
+                0),
+            feed::parser::error::CParseError);
+    };
+};
 
 TEST_MODULE(test_wind_parser, {
     test("valid msg", [] {
@@ -137,7 +139,7 @@ TEST_MODULE(test_wind_parser, {
         object::Wind wind;
         std::string  mwv("$WIMWV,242.8,R,6.9,N,A*20\r\n");
         assertTrue(windParser.unpack(mwv, wind));
-        assertEquals(wind.get_serialized(), mwv);
+        ASSERT_EQ(wind.get_serialized(), mwv);
     });
     test("invalid msg", [] {
         WindParser   windParser;
@@ -154,8 +156,8 @@ TEST_MODULE(test_atmosphere_parser, {
         object::Atmosphere atm;
         std::string        mda("$WIMDA,29.7987,I,1.0091,B,14.8,C,,,,,,,,,,,,,,*3E\r\n");
         assertTrue(atmParser.unpack(mda, atm));
-        assertEquals(atm.get_pressure(), 1009.1);
-        assertEquals(atm.get_serialized(), mda);
+        ASSERT_EQ(atm.get_pressure(), 1009.1);
+        ASSERT_EQ(atm.get_serialized(), mda);
     });
     test("invalid msg", [] {
         AtmosphereParser   atmParser;
@@ -178,13 +180,13 @@ TEST_MODULE(test_gps_parser, {
             gpsParser.unpack("$GPGGA,183552,5000.0466,N,00815.7555,E,1,05,1,105,M,48.0,M,,*49\r\n", pos));
         assertTrue(
             gpsParser.unpack("$GPGGA,183552,5000.0466,S,00815.7555,W,1,05,1,105,M,48.0,M,,*46\n", pos));
-        assertEquals(pos.get_dilution(), 1.0);
-        assertEquals(pos.get_fixQuality(), 1);
-        assertEquals(pos.get_nrOfSatellites(), 5);
-        assertEquals(pos.get_geoid(), 48.0);
-        assertEquals(pos.get_position().latitude, -math::dmToDeg(5000.0466));
-        assertEquals(pos.get_position().longitude, -math::dmToDeg(815.7555));
-        assertEquals(pos.get_position().altitude, 105);
+        ASSERT_EQ(pos.get_dilution(), 1.0);
+        ASSERT_EQ(pos.get_fixQuality(), 1);
+        ASSERT_EQ(pos.get_nrOfSatellites(), 5);
+        ASSERT_EQ(pos.get_geoid(), 48.0);
+        ASSERT_EQ(pos.get_position().latitude, -math::dmToDeg(5000.0466));
+        ASSERT_EQ(pos.get_position().longitude, -math::dmToDeg(815.7555));
+        ASSERT_EQ(pos.get_position().altitude, 105);
     });
     test("invalid msg", [] {
         GpsParser           gpsParser;
