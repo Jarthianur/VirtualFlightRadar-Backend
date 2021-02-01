e97a4c1f8797ceca5c8a163b9712bcb15db12651
Jarthianur <jarthianur.github@gmail.com>
2021-02-01 16:24:15 +0000

    fix profiler compile

--- profiling/profiler.hpp
+++ profiling/profiler.hpp
@@ -28,7 +28,7 @@
 #include <map>
 #include <utility>
 
-#include "types.h"
+#include "Types.hpp"
 
 namespace vfrb::profiling
 {
@@ -48,53 +48,47 @@ class Profiler
     char const*                          m_descr = nullptr;
 
     Profiler(std::function<void(Profiler&)>&& fn, usize size, char const* descr)
-        : m_testSize(size), m_fn(std::move(fn)), m_descr(descr)
-    {}
+        : m_testSize(size), m_fn(std::move(fn)), m_descr(descr) {}
 
     friend class ProfilerBuilder;
 
 public:
-    ~Profiler() noexcept = default;
-
-    void run()
-    {
+    void
+    run() {
         std::invoke(m_fn, *this);
     }
 
     template<typename FnT, typename... Args>
-    void measure(FnT&& fn, Args&&... args)
-    {
+    void
+    measure(FnT&& fn, Args&&... args) {
         auto t1 = clock();
         std::invoke(std::forward<FnT>(fn), std::forward<Args>(args)...);
         m_tickCount += clock() - t1;
     }
 
     template<typename FnT>
-    void profile(FnT&& fn)
-    {
-        for (usize x = 0; x < std::max<usize>(1, m_testSize / 2); ++x)
-        {
+    void
+    profile(FnT&& fn) {
+        for (usize x = 0; x < std::max<usize>(1, m_testSize / 2); ++x) {
             std::invoke(std::forward<FnT>(fn));
         }
         m_tickCount = 0;
-        for (usize x = 0; x < m_testSize; ++x)
-        {
+        for (usize x = 0; x < m_testSize; ++x) {
             std::invoke(std::forward<FnT>(fn));
         }
     }
 
-    f64 avgTime(TimeVal t) const
-    {
+    f64
+    avgTime(TimeVal t) const {
         return ((static_cast<f64>(m_tickCount) / static_cast<f64>(m_testSize)) /
                 static_cast<f64>(CLOCKS_PER_SEC)) *
                static_cast<f64>(t);
     }
 
-    void print(TimeVal t) const
-    {
+    void
+    print(TimeVal t) const {
         std::cout << m_descr << " - avg. duration: " << std::fixed << std::setprecision(6) << avgTime(t);
-        switch (t)
-        {
+        switch (t) {
             case TimeVal::SECONDS: std::cout << " s"; break;
             case TimeVal::MILLIS: std::cout << " ms"; break;
             case TimeVal::MICROS: std::cout << " µs"; break;
@@ -112,22 +106,19 @@ class ProfilerGroup
     friend class ProfilerBuilder;
 
 public:
-    ProfilerGroup(char const* name) : m_name(name) {}
-    ~ProfilerGroup() noexcept = default;
+    explicit ProfilerGroup(char const* name) : m_name(name) {}
 
-    void run()
-    {
-        for (auto& p : m_profilers)
-        {
+    void
+    run() {
+        for (auto& p : m_profilers) {
             p.run();
         }
     }
 
-    void print(TimeVal t) const
-    {
+    void
+    print(TimeVal t) const {
         std::cout << m_name << ": " << std::endl;
-        for (auto const& p : m_profilers)
-        {
+        for (auto const& p : m_profilers) {
             p.print(t);
         }
         std::cout << std::endl;
@@ -142,43 +133,42 @@ class ProfilerBuilder
     char const*                    m_group = nullptr;
 
 public:
-    ProfilerBuilder()           = default;
-    ~ProfilerBuilder() noexcept = default;
+    ProfilerBuilder() = default;
 
-    static std::map<char const*, ProfilerGroup>& profilers()
-    {
+    static std::map<char const*, ProfilerGroup>&
+    profilers() {
         static std::map<char const*, ProfilerGroup> profiler_list;
         return profiler_list;
     }
 
-    ProfilerBuilder const& build()
-    {
+    ProfilerBuilder const&
+    build() {
         profilers()
             .emplace(m_group, ProfilerGroup(m_group))
             .first->second.m_profilers.push_back(Profiler(std::move(m_fn), m_testSize, m_descr));
         return *this;
     }
 
-    ProfilerBuilder& withTestSize(usize size)
-    {
+    ProfilerBuilder&
+    withTestSize(usize size) {
         m_testSize = size;
         return *this;
     }
 
-    ProfilerBuilder& withTestFn(std::function<void(Profiler&)>&& fn)
-    {
+    ProfilerBuilder&
+    withTestFn(std::function<void(Profiler&)>&& fn) {
         m_fn = std::move(fn);
         return *this;
     }
 
-    ProfilerBuilder& withDescription(char const* descr)
-    {
+    ProfilerBuilder&
+    withDescription(char const* descr) {
         m_descr = descr;
         return *this;
     }
 
-    ProfilerBuilder& withGroup(char const* group)
-    {
+    ProfilerBuilder&
+    withGroup(char const* group) {
         m_group = group;
         return *this;
     }
